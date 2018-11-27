0846cd3f6d820e3fb16add193922d7cadf3ede6c
kh1267 <39156791+kh1267@users.noreply.github.com>
2018-11-27 22:32:01 +0900

    css complete

--- todo.js
+++ todo.js
@@ -33,7 +33,7 @@ function paintToDo(text) {
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
-  delBtn.innerText = "x";
+  delBtn.innerText = "완료";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
